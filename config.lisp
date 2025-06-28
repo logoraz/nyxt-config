0acff1ced2d9aadea2266bead5f3309670d0845b
Erik P Almaraz <erikalmaraz@fastmail.com>
2025-06-28 06:51:47 -0700

    chore updates
     - Update bookmarks
     - update gnome desktop -> new binary location
     - clean up config.lisp

--- config.lisp
+++ config.lisp
@@ -2,12 +2,12 @@
   (:use :cl :nyxt))
 (in-package :nxconfig-init)
 
-;;Add nyxt-config to ASDF registery and load nyxt-config system
+
 (progn
   (asdf:initialize-source-registry
    (list :source-registry
          (list :tree (uiop:xdg-config-home "nyxt/"))
-         #+or
-         (list :tree (uiop:xdg-data-home "common-lisp/bin/nyxt/"))
+         #+nil
+         (list :tree (uiop:xdg-data-home "nyxt/extensions/"))
          :inherit-configuration))
   (asdf:load-system :nxconfig))
